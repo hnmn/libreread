f02b8d245e08725f2260666c66b81667d27d079a
mysticmode <yo@mysticmode.org>
2017-12-29 15:47:26 +0530

    add set -e to install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with LibreRead.  If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+
 echo Please enter your domain address, Eg: www.example.com
 read domain_address
 
